41efc446fe96323931c40673ca8908656ed09304
valoeghese <46117728+valoeghese@users.noreply.github.com>
2025-10-29 16:56:08 +1300

    feat: Move polling to core1

--- code/Matrix_test1/main.cpp
+++ code/Matrix_test1/main.cpp
@@ -31,7 +31,8 @@ const uint8_t * current_char;
 enum disp_mode{
     USER = 0,
     ECSE = 1,
-    EASTER = 2
+    EASTER = 2,
+    TETRIS
 };
 
 disp_mode display_mode = USER;
@@ -51,13 +52,15 @@ char * strings[] = {
 };
 uint8_t tempBufferIdx = 0;
 void scroll_screen(void){
-    scroll_chars();
-    scroll_count++;
-    if(scroll_count==7){
-        counter = (counter+1) % strlen(strings[display_mode]);
-        const uint8_t * disp_char = char_to_matrix(strings[display_mode][counter]);
-        add_char_to_scroll(disp_char);
-        scroll_count=5-((disp_char[0]&0xE0)>>5); //3MSB of first col of char = length (0-7)
+    if (display_mode < TETRIS) {
+        scroll_chars();
+        scroll_count++;
+        if(scroll_count==7){
+            counter = (counter+1) % strlen(strings[display_mode]);
+            const uint8_t * disp_char = char_to_matrix(strings[display_mode][counter]);
+            add_char_to_scroll(disp_char);
+            scroll_count=5-((disp_char[0]&0xE0)>>5); //3MSB of first col of char = length (0-7)
+        }
     }
 }
 
@@ -89,6 +92,7 @@ void print_info(void){
     printf("---------------------------------------\n");
 }
 
+// debouncing
 bool Smooth(const bool raw, bool * const arr18)
 {
 #define idx ((unsigned char*)arr18)[16]
@@ -127,6 +131,37 @@ bool Smooth(const bool raw, bool * const arr18)
 #undef idx
 #undef val
 
+// Core 1: Poll GPIO inputs
+void Poll(void) {
+    static bool pb1_arr[18] = { true }; // nb this only sets [0] to true
+    pb1_arr[17] = true;
+    static bool pb2_arr[18] = { true };
+    pb1_arr[17] = true;
+    
+    for (;;) {
+        static bool pb1_last = 1, pb2_last = 1;
+        
+        bool pb1_val = Smooth(gpio_get(PB1), pb1_arr);
+        bool pb2_val = Smooth(gpio_get(PB2), pb2_arr);
+
+        // on edge, change
+        if((pb1_last != pb1_val)||(pb2_last!=pb2_val)){
+            if((pb1_val==0) &&(pb2_val ==0)){
+                multicore_fifo_push_blocking(EASTER);
+            } else if (!pb1_val) {
+                multicore_fifo_push_blocking(TETRIS);
+            } else if (!pb2_val) {
+                static bool toggle = 0;
+
+                multicore_fifo_push_blocking((toggle ^= 1) ? USER : ECSE);
+            }
+        }
+
+        pb1_last = pb1_val;
+        pb2_last = pb2_val;
+    }
+}
+
 int main()
 {
     tempBuffer[0] = ' ';
@@ -137,61 +172,36 @@ int main()
     screen_start();
     printf("hello, world!");
     add_repeating_timer_ms(-100,scroll_timer_cb,0,&scroll_timer);
+    multicore_launch_core1(Poll);
     
     while (true) {
-        static bool pb1_arr[18];
-        static bool pb2_arr[18];
-
-        static bool pb1_last = 1, pb2_last = 1;
+        uint32_t r;
         
-        bool pb1_val = gpio_get(PB1);// Smooth(gpio_get(PB1), pb1_arr);
-        bool pb2_val = gpio_get(PB2);//Smooth(gpio_get(PB2), pb2_arr);
+        if (multicore_fifo_pop_timeout_us(10, &r)) {
+            display_mode = (disp_mode)r;
 
-        if((pb1_last != pb1_val)||(pb2_last!=pb2_val)){
-            if((pb1_val==0) &&(pb2_val ==0)){
-                display_mode = EASTER;
-                counter = 0;
+            //3MSB of first col of char = length (0-7)
+            counter = 0;
+            if (display_mode < TETRIS) {
                 const uint8_t * disp_char = char_to_matrix(strings[display_mode][counter]);
                 add_char_to_scroll_start(disp_char);
                 add_char_to_scroll(disp_char);
-                scroll_count=5-((disp_char[0]&0xE0)>>5); //3MSB of first col of char = length (0-7)
-            }else if((pb1_val == 0)){
-                display_mode = ECSE;
-                counter = 0;
-                const uint8_t * disp_char = char_to_matrix(strings[display_mode][counter]);
-                add_char_to_scroll_start(disp_char);
-                add_char_to_scroll(disp_char);
-                scroll_count=5-((disp_char[0]&0xE0)>>5); //3MSB of first col of char = length (0-7)
-                print_info();
-            }else if(pb2_val == 0){
-                static bool toggle = 0;
-                display_mode = (toggle ^= 1) ? USER : ECSE;
-                counter = 0;
-                const uint8_t * disp_char = char_to_matrix(strings[display_mode][counter]);
-                add_char_to_scroll_start(disp_char);
-                add_char_to_scroll(disp_char);
-                scroll_count=5-((disp_char[0]&0xE0)>>5); //3MSB of first col of char = length (0-7)
+                scroll_count=5-((disp_char[0]&0xE0)>>5);
             }
-            
-            //add_char_to_scroll(char_to_matrix(stringBuffer[counter]));
         }
-        pb1_last = pb1_val;
 
+        // Clear when holding down pb
         if(gpio_get(PB2)){
             current_char=scroll_buff;//char_to_matrix(stringBuffer[counter]);
         }
-        else{
-            //scroll_screen();
-            //current_char = char_to_matrix(128);
-            //print_print_buff();
-        }
+
         for(int i = 0; i < 100; i++){
             disp_char(current_char); 
             //This is janky - ISRs were being weird so we just do 100 display cycles for every button poll
             //which means our polling rate is worst case 100us*25*100 = 250ms.
             //if ISRs still funky maybe throw this on core 1? would be cool and leave core 0 available for user code/polling.
         }
-        //scroll_screen();
+
         char inChar = getchar_timeout_us(10);
         if(inChar != 0xFE){
             if(inChar != '\n'){
